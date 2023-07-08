360ad498a3bfbd419b192cfdb317c999411ac85e
cd-x <whowantsthisemailid@gmail.com>
2023-07-08 17:43:11 +0530

    initial commit random roll working fine

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,28 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var dice1 = 0
+    var dice2 = 5
+    var images = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = images[dice1]
+        diceImageView2.image = images[dice2]
+        diceImageView1.alpha = 1
+        diceImageView2.alpha = 1
     }
 
-
+    @IBAction func rollButtonHandler(_ sender: Any) {
+        diceImageView1.alpha = 0.5
+        diceImageView2.alpha = 0.5
+        dice1 = Int.random(in: 0...5)
+        dice2 = Int.random(in: 0...5)
+        viewDidLoad()
+    }
+    
 }
 
